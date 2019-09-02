d807bb2db8540b1e1816aafcfbeceff0dc91fcc2
sasi-eShopping <sasi2212kumar@gmail.com>
2019-09-02 18:50:13 +0530

    [new entry]

---
+++ src/main/java/com/example/demo/service/ProductController.java
@@ -0,0 +1,60 @@
+package com.example.demo.service;
+
+
+
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+
+
+@Controller    // This means that this class is a Controller
+@RequestMapping(path="/products") // This means URL's start with /demo (after Application path)
+public class ProductController {
+
+    
+	@GetMapping(path="/getProduct/{id}")
+	public @ResponseBody Product  getProduct(@PathVariable("id") String id) throws Exception {
+	System.out.println("HIIIII");
+	if(id.equalsIgnoreCase("1"))
+	{
+		Thread.sleep(500);
+		System.out.println("500");
+	}
+	else if(id.equalsIgnoreCase("2"))
+	{
+		Thread.sleep(1000);
+		System.out.println("1000");
+	}
+	else if(id.equalsIgnoreCase("3"))
+	{
+		Thread.sleep(2000);
+		System.out.println("2000");
+	}
+	else
+	{
+	Thread.sleep(4000);
+	System.out.println("4000");
+	}
+	//Product prod=new Product("1","Pen","12");
+	return  new Product("1","Pen","12");
+	
+	}
+
+
+}
